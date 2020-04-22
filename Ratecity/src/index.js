f6f25c5a07c72be62d76b2deee2f416a5c36cb9d
Shreeya <54701291+Bidkar2912@users.noreply.github.com>
2020-04-22 14:56:54 +0530

    Delete index.js

--- Ratecity/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-
-ReactDOM.render(<App />, document.getElementById('App'));
